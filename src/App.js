24d4780ab5094e04610bf690901df5a5cd1c72f1
brijesh.chhabra <brijesh.chhabra@wirecard.com>
2020-05-04 00:07:23 +0530

    alternative to bind

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ class App extends Component {
       ]
     });
   };
-
+  
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Nidhi')}>Switch Name</button>
+        <button onClick={ () => this.switchNameHandler('BBChhabra !!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -37,7 +37,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Bunty')}
+          click={this.switchNameHandler.bind(this, 'Nidhi')}
         >
           My Hobbies: Racing
         </Person>
