13f3ec3080cad171daedb7c2615c0631331970dc
brijesh.chhabra <brijesh.chhabra@wirecard.com>
2020-05-03 23:01:27 +0530

    Infinite loop Error: Too many re-renders

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component { 
-  state = { 
-    persons: [
-      { name: 'Brijesh', age: 34},
-      { name: 'Aleksej', age: 34},
-      { name: 'Saurabh', age: 34},
-    ]
-  }
-
-  switchNameHandler = () =>{
-    //console.log('Was clicked !!');
-    this.setState({
+const App = props => { 
+  const [ personsState, setPersonsState ] = useState({
       persons: [
-        { name: 'Brijesh Chhabra', age: 34},
+        { name: 'Brijesh', age: 34},
         { name: 'Aleksej', age: 34},
-        { name: 'Saurabh', age: 38},
+        { name: 'Saurabh', age: 34},
       ],
-      otherState: 'Some Other value'
-
     });
-  }
-  render() {  
-  return (
-   <div className="App">
-       <h1> Hi I am Brijesh !!</h1>
-       <p> this is really working</p>
-       <button onClick={this.switchNameHandler}> Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-   </div>
-  );
-    //return React.createElement('div', null, React.createElement('h1', null,  'Does this work now?'));
-  }
-}
+
+ const switchNameHandler = () => {
+  setPersonsState({
+    persons: [
+      { name: 'Brijesh Chhabra', age: 34},
+      { name: 'Aleksej', age: 34},
+      { name: 'Saurabh', age: 38},
+    ]
+  });
+};
+ return (
+  <div className="App">
+      <h1> Hi I am Brijesh !!</h1>
+      <p> this is really working</p>
+      <button onClick={switchNameHandler()}> Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+  </div> );
+
+};
 
 export default App;
+
+
