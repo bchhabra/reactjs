e4b935dd43b856f74c2f1a4965bb49b9763753e4
Nilay <nilaych@gmail.com>
2020-05-11 03:07:32 +0530

    code refactoring

--- src/util/MyPanel.jsx
+++ src/util/MyPanel.jsx
@@ -14,38 +14,42 @@ class MyPanel extends React.Component {
         return (
         <div>
             <button id='btn-ok' onClick={this.clickedHandler}>Ok</button>
-            <button id='btn-cancel' onClick={this.clickedHandler}>Cancel</button>
+            <button id='btn-cancel' onClick={this.clickedHandler}>Cancel</button><br />
 
-
-            <button id='btn-service' onClick={this.handleServiceCall.bind(this)}>Call service</button>
-            Ok clicked: <label>{this.state.okCount}</label>
-            Cancel clicked: <label>{this.state.cancelCount}</label>
+            Ok clicked: <label>{this.state.okCount}</label><br />
+            Cancel clicked: <label>{this.state.cancelCount}</label><br />
+            
+            <button id='btn-service' 
+                onClick={this.handleServiceCall.bind(this)}>Call service</button><br />
+            
             The data from the service is :
             <pre>
-                {JSON.stringify(this.state.data)}
+                {JSON.stringify(this.state.data, null, 2)}
             </pre>   
 
-
-            <SizeSelector sizes = {['S', 'M', 'L', 'XL', 'XXL']} onSelect={this.handleSelection}/>
+            <SizeSelector 
+                sizes = {['S', 'M', 'L', 'XL', 'XXL']} 
+                onSelect={this.handleSizeSelection}
+                selected={this.state.selectedSize} />
         </div>);
     }
 
-    handleSelection = (value) => {
-        console.log("Something selected :: " + value);
+    handleSizeSelection = (value) => {
+        console.log("Size selected :: " + value);
+        this.setState({selectedSize: value});
     }
 
 
     clickedHandler(event) {
         if(event.target.id == 'btn-ok') {
             this.setState({okCount: this.state.okCount + 1});
-        } else {
+        } else if(event.target.id == 'btn-cancel') {
             this.setState({cancelCount: this.state.cancelCount + 1});
         }
     }
 
     handleServiceCall() {
         fetch('http://api.github.com/')
-
         .then(response => response.json())
         .then(data => this.setState({data}));
     }
--- src/util/SizeSelector.js
+++ src/util/SizeSelector.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
-const SizeSelector = ({sizes = [], onSelect}) => {
-
+const SizeSelector = ({sizes = [], onSelect, selected}) => {
 
     const propagate = (event) => {
-        console.log("Size selected");
-        onSelect(sizes[event.target.id]);  //Calling point
+        onSelect(sizes[event.target.id]);  //Calling the actual handler
     }
 
-
-    return (<div>
+    return (
+        <div>
+            {
+                sizes.map((size, index) => 
+                    <button 
+                        id={index}
+                        key={index} 
+                        style={{marginRight: '5px', borderColor: (selected == sizes[index] ? 'blue' : null)}} 
+                        onClick={propagate}>{size}</button>
+                )
+            }
+            
+            {/**
             <button id='0' onClick={propagate}>{sizes[0]}</button>
             <button id='1' onClick={propagate}>{sizes[1]}</button>
             <button id='2' onClick={propagate}>{sizes[2]}</button>
             <button id='3' onClick={propagate}>{sizes[3]}</button>
             <button id='4' onClick={propagate}>{sizes[4]}</button>
+            */}
         </div>
     );
-
-   
-
 }
 
 export default SizeSelector;
