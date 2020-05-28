aad8e35e2c6b875a1dfb36d1c497701e79e05120
Nilay <nilaych@gmail.com>
2020-05-28 14:05:10 +0530

    Exercise added

---
+++ exercise.js
@@ -0,0 +1,60 @@
+const array1 = [1, 2, 3, 4, 5]
+const array2 = [6, 7, 8, 9, 20]
+// 1. WAP to merge only the even numbers from the two arrays to a resultant array
+const result = [] //write code here
+console.log(result)
+
+const array = [1, 2, 3, 4, 5]
+const sum = 0 //write code here
+// 2. WAP to calculate sum of all the elements in the array
+console.log(sum)
+
+const employees = [
+    {
+        id: 1,
+        role: 'Developer',
+        salary: 100000
+    },
+    {
+        id: 2,
+        role: 'Developer',
+        salary: 200000
+    },
+    {
+        id: 3,
+        role: 'Team Lead',
+        salary: 300000
+    }
+]
+// 3. WAP to calculate total salary
+const totalSalary = 0 // Write code here
+console.log('Total salary is : ' + totalSalary)
+
+// 4. WAP to get only the ids from the employees array
+const empIds = [] // Write code here
+console.log('Employee ids are : ' + empIds)
+
+// 5. WAP to find the employee with max salary
+const empWithMaxSalary = {} // Write code here
+console.log('Employe with max salary : ' + JSON.stringify(empWithMaxSalary, null, 2));
+
+// 6. WAP to find the employees with Developer role
+const developers = [] // Write code here
+console.log('Developers are : ' + developers);
+
+// 7. WAP to create employees out of the employees array with double salary 
+// and an additional field division = 'Issuining'
+const issuingEmps = [] // Write code here
+console.log('Issuing employees : ' + issuingEmps)
+
+// 8. WAP to get only the developr's ids
+const developerIds = [] // Write code here
+console.log('Developer ids are : ' + developerIds)
+
+// 9. WAP to delete the employee with Team Lead role from the array
+const empsWithoutTeamLead = [] // Write code here
+console.log('Employees without team lead : ' + empsWithoutTeamLead)
+
+// 10. WAP to add a new employee without changing the existing array
+const newEmpList = []
+console.log('New employees are : ' + newEmpList)
